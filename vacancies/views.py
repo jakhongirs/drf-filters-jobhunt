51757d78260c5c4070fdb5af26333ecc0e3206c8
jakhongirs <j.sagdullaev31@gmail.com>
2022-07-26 15:52:11 +0500

    feat: customVacancyFilter

--- vacancies/views.py
+++ vacancies/views.py
@@ -7,8 +7,16 @@ from helpers.pagination import CustomPagination
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 
+from django_filters import rest_framework
+
 
 # Create your views here.
+class CustomVacancyFilter(filters.BaseFilterBackend):
+    min_price = rest_framework.NumberFilter(field_name="min_salary", lookup_expr='gte', label="Minimum salary")
+    max_price = rest_framework.NumberFilter(field_name="max_salary", lookup_expr='lte', label="Maximum salary")
+
+    created_at = rest_framework.DateTimeFromToRangeFilter(field_name='created_at', label="Created date range")
+
 
 # ALL VACANCIES:
 class AllVacanciesView(generics.ListAPIView):
@@ -16,7 +24,7 @@ class AllVacanciesView(generics.ListAPIView):
     serializer_class = VacancySerializer
     pagination_class = CustomPagination
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
-    filterset_fields = ('status', 'region__slug', 'category')
+    filterset_fields = ('status', 'region', 'region__slug', 'category')
     search_fields = ('title',)
 
 
